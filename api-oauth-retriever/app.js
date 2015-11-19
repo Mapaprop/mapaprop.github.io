15cbe26d00957da6e0f359f331f3a669c25c8e4d
Rodrigo Asensio <rodrigo.asensio@gilbarco.com>
2015-11-19 18:31:37 -0500

    api oauth retriever

---
+++ api-oauth-retriever/app.js
@@ -0,0 +1,26 @@
+(function() {
+  var app = angular.module('api-oauth-retriever', []);
+})();
+
+angular.module('api-oauth-retriever').controller('PageController', ['$scope', '$http', function($scope, $http) {
+
+  $scope.clientId = '';
+  $scope.clientSecret = '';
+
+  // retrieve function
+  $scope.retrieveToken = function() {
+		$scope.token = '';
+		$scope.error = '';
+    var params = 'client_id=' + $scope.clientId + '&client_secret=' + $scope.clientSecret + '&grant_type=client_credentials';
+    $http.post('https://www.mapapropapp.com/api/action/oauth2-v1/authorize').success(function(data) {
+//    $http.post('http://localhost:8080/api/action/oauth2-v1/authorize', params, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    }).success(function(data) {
+      $scope.token = data.access_token;
+    }).error(function(data) {
+      $scope.error = data.description;
+    });
+  }
+}]);
