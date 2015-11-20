eb4c54b5e7ad9104394fb3e5e8e9135554784fcc
Rodrigo Asensio <rodrigo.asensio@gilbarco.com>
2015-11-19 19:22:09 -0500

    Fixed issue with token url

--- api-oauth-retriever/app.js
+++ api-oauth-retriever/app.js
@@ -13,7 +13,6 @@ angular.module('api-oauth-retriever').controller('PageController', ['$scope', '$
 		$scope.error = '';
     var params = 'client_id=' + $scope.clientId + '&client_secret=' + $scope.clientSecret + '&grant_type=client_credentials';
     $http.post('https://www.mapapropapp.com/api/action/oauth2-v1/authorize').success(function(data) {
-//    $http.post('http://localhost:8080/api/action/oauth2-v1/authorize', params, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
       }
